8f96a662ba6ba398ff71cd6b58f9b8e6c04b28be
Dali-debug <mohamedali.bensalah@supcom.tn>
2025-11-20 22:58:02 +0100

    Garderie inscription

--- lib/models/nursery.dart
+++ lib/models/nursery.dart
@@ -4,6 +4,8 @@ class Nursery {
   final String id;
   final String name;
   final String address;
+  final String? city;
+  final String? postalCode;
   final double distance;
   final double rating;
   final int reviewCount;
@@ -14,14 +16,20 @@ class Nursery {
   final String photo;
   final String description;
   final List<String> activities;
+  final List<String>? facilities;
   final int staff;
   final String ageRange;
+  final String? phone;
+  final String? email;
+  final String? ownerId;
   final List<Review>? reviews;
 
   Nursery({
     required this.id,
     required this.name,
     required this.address,
+    this.city,
+    this.postalCode,
     required this.distance,
     required this.rating,
     required this.reviewCount,
@@ -32,8 +40,12 @@ class Nursery {
     required this.photo,
     required this.description,
     required this.activities,
+    this.facilities,
     required this.staff,
     required this.ageRange,
+    this.phone,
+    this.email,
+    this.ownerId,
     this.reviews,
   });
 
@@ -42,6 +54,8 @@ class Nursery {
       'id': id,
       'name': name,
       'address': address,
+      'city': city,
+      'postalCode': postalCode,
       'distance': distance,
       'rating': rating,
       'reviewCount': reviewCount,
@@ -52,8 +66,12 @@ class Nursery {
       'photo': photo,
       'description': description,
       'activities': activities,
+      'facilities': facilities,
       'staff': staff,
       'ageRange': ageRange,
+      'phone': phone,
+      'email': email,
+      'ownerId': ownerId,
       'reviews': reviews?.map((r) => r.toJson()).toList(),
     };
   }
@@ -63,6 +81,8 @@ class Nursery {
       id: json['id'],
       name: json['name'],
       address: json['address'],
+      city: json['city'],
+      postalCode: json['postalCode'],
       distance: json['distance'].toDouble(),
       rating: json['rating'].toDouble(),
       reviewCount: json['reviewCount'],
@@ -73,8 +93,14 @@ class Nursery {
       photo: json['photo'],
       description: json['description'],
       activities: List<String>.from(json['activities']),
+      facilities: json['facilities'] != null
+          ? List<String>.from(json['facilities'])
+          : null,
       staff: json['staff'],
       ageRange: json['ageRange'],
+      phone: json['phone'],
+      email: json['email'],
+      ownerId: json['ownerId'],
       reviews: json['reviews'] != null
           ? (json['reviews'] as List).map((r) => Review.fromJson(r)).toList()
           : null,
